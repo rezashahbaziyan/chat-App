422216cef40cb303667146a627bce531006db03d
ujin <rezashahtrader@gmail.com>
2023-02-15 18:55:08 +0330

    Create components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
-import { ChatEngine } from "react-chat-engine";
-import "./App.css";
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+
     return (
         <ChatEngine
             height="100vh"
-            projectID="397e5561-8e68-4fcc-ab9a-9fa272b64c42"
-            userName="Rezashahbaziyan"
-            userSecret="123123"
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     );
-}
+};
+
+// infinite scroll, logout, more customizations...
+
 export default App;

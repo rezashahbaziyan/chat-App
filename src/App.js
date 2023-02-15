6f367d226fac850717ab9e0f74709de6faeaf378
ujin <rezashahtrader@gmail.com>
2023-02-15 16:59:30 +0330

    set Chat Engine

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
-export default function App() {
-
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="397e5561-8e68-4fcc-ab9a-9fa272b64c42"
+            userName="Rezashahbaziyan"
+            userSecret="123123"
+        />
+    );
 }
+export default App;
